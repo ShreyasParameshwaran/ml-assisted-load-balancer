57f5db32c4d7d1d58cfd70d312ce653ab2a40762
sid-js <sidhsreejil@gmail.com>
2024-08-23 00:52:53 +0530

    Fix Export Config

--- setup_minikube.sh
+++ setup_minikube.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Function to open a new terminal and run a command
+open_new_terminal() {
+    local command="$1"
+    gnome-terminal -- bash -c "$command; exec bash"
+}
+
 # Start Minikube
 echo "Starting Minikube..."
 minikube start
@@ -13,22 +19,22 @@ find . -name '*.yaml' -exec kubectl apply -f {} \;
 
 # Run minikube tunnel in a new terminal
 echo "Starting minikube tunnel..."
-gnome-terminal -- bash -c "minikube tunnel"
+open_new_terminal "minikube tunnel"
 
 # Run port-forward for Prometheus in a new terminal
 echo "Starting port-forward for Prometheus..."
-gnome-terminal -- bash -c "kubectl port-forward service/prometheus-kube-prometheus-prometheus 9090:9090 -n monitoring"
+open_new_terminal "kubectl port-forward service/prometheus-kube-prometheus-prometheus 9090:9090 -n monitoring"
 
 # Run port-forward for Ingress-NGINX in a new terminal
 echo "Starting port-forward for Ingress-NGINX..."
-gnome-terminal -- bash -c "kubectl port-forward service/ingress-app-ingress-nginx-controller 3000:80"
+open_new_terminal "kubectl port-forward service/ingress-app-ingress-nginx-controller 3000:80"
 
 # Run port-forward for ML API service in a new terminal
 echo "Starting port-forward for ML API service..."
-gnome-terminal -- bash -c "kubectl port-forward service/ml-api-service 5000:5000"
+open_new_terminal "kubectl port-forward service/ml-api-service 5000:5000"
 
 # Run kubectl get all in a new terminal
 echo "Running kubectl get all..."
-gnome-terminal -- bash -c "kubectl get all"
+open_new_terminal "kubectl get all"
 
 echo "All commands have been executed."
